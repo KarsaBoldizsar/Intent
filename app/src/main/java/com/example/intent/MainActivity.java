9c4f50d60c04fddf46d992d2360f7c5234f6c84a
Diak <karsa.boldi@gmai.com>
2019-09-26 16:43:35 +0200

    orimunka_1. feladat

--- app/src/main/java/com/example/intent/MainActivity.java
+++ app/src/main/java/com/example/intent/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonElso;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        init();
+
+        buttonElso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent masodik_activityre = new Intent(MainActivity.this,nemtommi.class);
+                startActivity(masodik_activityre);  //ezzel iondítod el az intent változót
+                finish();                           //ezzel zárjuk az első alkalmazást
+            }
+        });
+        }
+
+    public void init()
+    {
+        buttonElso = findViewById(R.id.button_elso);
+
     }
 }
